4e30701cfcbc495f676efd6f5e44953dc9459d40
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2023-02-01 16:57:31 +0100

    creation/reprise de monde par joueur

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ let world = require("./world");
 
 
 const server = new ApolloServer({ typeDefs, resolvers, context: async ({ req }) => ({
-    world: world
+    world: world,
+    world: await readUserWorld(req.headers["x-user"]),
+    user: req.headers["x-user"]
     })
-})
+});
+
 const app = express();
 app.use(express.static('public'));
 server.start().then( res => {
     server.applyMiddleware({app});
     app.listen({port: 4000}, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
-})
+});
+
+async function readUserWorld(user) {
+    try {
+        const data = await fs.readFile("userworlds/"+ user + "-world.json");
+        return JSON.parse(data);
+    }
+    catch(error) {
+        return world;
+    }
+}
+   
--- resolvers.js
+++ resolvers.js
@@ -1,10 +1,41 @@
 module.exports = {
     Query: {
-        getWorld(parent, args, context) {
+        getWorld(parent, args, context, info) {
+            saveWorld(context)
             return context.world
         }
     },
     Mutation: {
+        acheterQtProduit(id, quantite){
+            /*       Cf page 39 sujet
+            getproduitById(args.id)
+            if existe
+                for(int i=1;i<args.quantite;i++){}
+                    setMoney(getMoney-getproduitById(args.id).cout)
+                    setNextPriceProduct();
+                    setQteProduit(getQteProduit+1)
+                }
+            else
+                throw new Error(`Le produit avec l'id ${args.id} n'existe pas`)
+            */
+
+               
+        },
+        lancerProductionProduit(id){
+
+        },
+        engagerManager(name){
+
+        }
     }
 };
+
+function saveWorld(context) {
+    fs.writeFile("userworlds/" + context.user + "-world.json", JSON.stringify(context.world), err => {
+        if (err) {
+            console.error(err)
+            throw new Error(`Erreur d'écriture du monde coté serveur`)
+        }
+    })
+}
    
