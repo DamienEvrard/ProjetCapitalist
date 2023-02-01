38519846b7b42c5da2607842f857e35aa13c26cc
DamienEvrard <damien.evrard@etud.univ-jfc.fr>
2023-02-01 16:12:50 +0100

    ajout d'icones

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const { ApolloServer, gql } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL schema language
@@ -13,6 +14,7 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers})
 const app = express();
+app.use(express.static('public'));
 server.start().then( res => {
     server.applyMiddleware({app});
     app.listen({port: 4000}, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
